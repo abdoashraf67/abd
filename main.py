0b55a52e4c401d687a0216a1601f8f0b3b5fb0fe
Abdo <abdel129130@gmail.com>
2024-04-15 10:33:04 +0200

    add new function

--- main.py
+++ main.py
@@ -10,3 +10,14 @@ def tests_add5():
     assert r == 10
     r = add5(10.102645)
     assert r == 15.102645
+from netmiko import ConnectHandler
+
+def get_running_version(driver, host, username="admin", password="admin"):
+    with ConnectHandler(
+        device_type=driver,
+        host=host,
+        username=username,
+        password=password
+    ) as device:
+        version = device.send_command("show version", use_textfsm=True)
+    return version
